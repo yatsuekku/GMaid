b1ccf393d0a64b9bc287d56faf0d8c6615d4709b
yatsuekku <yatsuekku97@gmail.com>
2017-05-07 02:03:01 +0200

    Add insertVertex() in GraphNote

--- Note/src/NoteGraph.java
+++ Note/src/NoteGraph.java
@@ -9,7 +9,7 @@ import java.util.TreeMap;
  */
 public class NoteGraph implements Note {
 
-    TreeMap<Pair<Note,Note>,ListNote> content; //Pair(a,b) in map represents edge a~b. Pair(a,a) represents vertex a;
+    private TreeMap<Pair<Note,Note>,ListNote> content; //Pair(a,b) in map represents edge a~b. Pair(a,a) represents vertex a;
     @Override
 
     public List<Note> getSubnotes() {
@@ -30,4 +30,18 @@ public class NoteGraph implements Note {
             }
         }
     }
+
+    public void insertVertex(Note a,Note c){
+        ListNote temp = content.get(new Pair<Note,Note>(a,a));
+        if(temp == null){
+            ListNote toInsert = new ListNote();
+            toInsert.append(c);
+            content.put(new Pair<Note,Note>(a,a),toInsert);
+        }else{
+            temp.append(c);
+            content.put(new Pair<Note,Note>(a,a),temp);
+        }
+    }
+
+
 }
